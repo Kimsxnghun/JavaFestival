880b563d5212da357550299d70ac1998af8c7a3d
Kimsunghun <rlatjdgns432@gmail.com>
2022-02-04 18:58:58 +0900

    - 14번 중복 없이 난수 뽑기

--- JavaFestival/src/ex2/Java14_2.java
+++ JavaFestival/src/ex2/Java14_2.java
@@ -2,21 +2,17 @@ package ex2;
 
 import java.util.Random;
 
-public class Java14 {
+public class Java14_2 {
 	public static void main(String[] agrs) {
 		// 중복 없이 숫자를 뽑는 로또 프로그램
 		
 		Random r = new Random();
 		int[] arr = new int[6];
-		int i = 0;
 		
 		// 값 삽입, 반복문은 배열 크기만큼 반복
-		// 반복 횟수가 정해져 있기 때문에 for문 사용하는 것이 더 좋을듯
-		while(i<arr.length) 
+		for(int i=0; i<arr.length; i++) 
 		{
-			// 배열에 난수 삽입
 			arr[i] = r.nextInt(6)+1;
-			
 			// 중복 값 처리
 			for(int j=0; j<i; j++) 
 			{
@@ -25,9 +21,6 @@ public class Java14 {
 					i--;
 				}
 			}
-			
-			// 다음 값을 넣기 위해 i++
-			i++;
 			
 		}
 		
