c1b3b83d108e0f343399e8e2f77ea83414439642
Kimsunghun <rlatjdgns432@gmail.com>
2022-02-08 10:29:27 +0900

    - 16번 가까운 값 찾기

--- JavaFestival/src/ex2/Java16.java
+++ JavaFestival/src/ex2/Java16.java
@@ -5,28 +5,39 @@ public class Java16 {
 		// 1차원 점들 중 가장 거리가 짧은 점(index)를 출력하기
 		int[] point = {92, 32, 52, 9, 81, 2, 68};
 		// 결과화면
-		// result = [3, 5] // 3번과 5번의 거리가 가장 짧다는 말인듯
+		// result = [3, 5] --> 3번과 5번의 거리가 가장 짧다
+
+		// 거리 값을 저장할 배열
+		int size = 0; // 배열 크기
+		for(int i=1; i<point.length; i++) 
+		{
+			size += i;
+		}
+		// System.out.println(size);
+		int[] dis = new int[size];
+
+		int min = 0; // dis 인덱스 번호
+		int a = 0; // point 인덱스 번호1
+		int b = 0; // point 인덱스 번호2
 		
-		// 거리 = 점 - 점
+		for(int i = 0, k = 0; i<point.length; i++) 
+		{
+			for(int j = i+1; j<point.length; j++, k++)
+			{
+				dis[k] = Math.abs(point[i]-point[j]); // 거리의 절대값
+				 
+				if(dis[min]>dis[k]) // 최소값 구하기
+				{
+					min = k; //** 작을 때 값을 바꿔줘야함
+					a = i;
+					b = j;
+				}
+			}
+		}
 		
-		// 0번 1번 비교
-		// 0번 2번 비교
-		// 0번 3번 비교
-		// 0번 4번 비교
-		// 0번 5번 비교
-		// 0번 6번 비교 - > 6회
-		
-		// 1번 2번 비교
-		// 1번 3번 비교 ... 
-		// 1번 6번 비교 -> 5회
-		
-		// 2번 -> 4회
-		
-		// 3번 -> 3회
-		
-		// ..
-		
-		// 6번 -> 1회
+		System.out.println(min); // 최소값인 경우 dis의 인덱스 번호
+		System.out.println(dis[min]); // 거리 최소값 
+		System.out.println("result = [" + a +", "+ b + "]"); // 최소값인 두 점
 		
 		
 	}
